63cf968a0d66a61dbb08c41dc9c17e03cc622a52
Haidar-Dzaky <adizaki10@gmail.com>
2021-02-15 19:55:31 +0700

    update

---
+++ app/src/main/java/com/haidar_18104011/sewpeda/detail.kt
@@ -0,0 +1,11 @@
+package com.haidar_18104011.sewpeda
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class detail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
